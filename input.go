03e157d1801c8aacc535671b7761ea094faf54a5
DeedleFake <deedlefake@users.noreply.github.com>
2022-05-08 13:54:13 -0400

    wlr: more implementation of input stuff

--- input.go
+++ input.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"os"
 	"time"
 
 	"deedles.dev/wlr"
+	"deedles.dev/wlr/xkb"
 )
 
 func (server *Server) onCursorMotion(dev wlr.InputDevice, t time.Time, dx, dy float64) {
@@ -26,10 +28,64 @@ func (server *Server) onCursorFrame() {
 	panic("Not implemented.")
 }
 
-func (server *Server) onNewInput(input wlr.InputDevice) {
+func (server *Server) onNewInput(device wlr.InputDevice) {
+	switch device.Type() {
+	case wlr.InputDeviceTypeKeyboard:
+		server.newKeyboard(device)
+	case wlr.InputDeviceTypePointer:
+		server.newPointer(device)
+	}
+
+	caps := wlr.SeatCapabilityPointer
+	if len(server.keyboards) != 0 {
+		caps |= wlr.SeatCapabilityKeyboard
+	}
+	server.seat.SetCapabilities(caps)
+}
+
+func (server *Server) newKeyboard(device wlr.InputDevice) {
+	kb := Keyboard{
+		Server: server,
+		Device: device,
+	}
+
+	rules := xkb.RuleNames{
+		Rules:   os.Getenv("XKB_DEFAULT_RULES"),
+		Model:   os.Getenv("XKB_DEFAULT_MODEL"),
+		Layout:  os.Getenv("XKB_DEFAULT_LAYOUT"),
+		Variant: os.Getenv("XKB_DEFAULT_VARIANT"),
+		Options: os.Getenv("XKB_DEFAULT_OPTIONS"),
+	}
+
+	ctx := xkb.NewContext(xkb.ContextNoFlags)
+	defer ctx.Unref()
+
+	keymap := xkb.NewKeymapFromNames(ctx, &rules, xkb.KeymapCompileNoFlags)
+	defer keymap.Unref()
+
+	wkb := device.Keyboard()
+	wkb.SetKeymap(keymap)
+	wkb.SetRepeatInfo(25, 600)
+
+	kb.Modifiers = wkb.OnModifiers(kb.onModifiers)
+	kb.Key = wkb.OnKey(kb.onKey)
+
+	server.seat.SetKeyboard(device)
+	server.keyboards = append(server.keyboards, kb)
+}
+
+func (server *Server) newPointer(device wlr.InputDevice) {
 	panic("Not implemented.")
 }
 
 func (server *Server) onRequestCursor(client wlr.SeatClient, surface wlr.Surface, serial uint32, hotspotX, hotspotY int32) {
+	panic("Not implemented.")
+}
+
+func (kb *Keyboard) onModifiers(keyboard wlr.Keyboard) {
+	panic("Not implemented.")
+}
+
+func (kb *Keyboard) onKey(keyboard wlr.Keyboard, t time.Time, code uint32, update bool, state wlr.KeyState) {
 	panic("Not implemented.")
 }
