721bab5bfcefeceaf84ecd041a67e14b34f250c4
DeedleFake <deedlefake@users.noreply.github.com>
2022-05-08 13:04:30 -0400

    internal/drm: add

---
+++ internal/drm/drm.go
@@ -0,0 +1,9 @@
+package drm
+
+//func fourccCode(a, b, c, d uint32) uint32 {
+//	return a | (b << 8) | (c << 16) | (d << 24)
+//}
+
+const (
+	FormatARGB8888 = 'A' | ('R' << 8) | ('2' << 16) | ('4' << 24)
+)
