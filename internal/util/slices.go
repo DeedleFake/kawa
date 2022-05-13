159af70eba5d7f4a9460841b5261b3031eeeb619
DeedleFake <deedlefake@users.noreply.github.com>
2022-05-13 16:34:07 -0400

    internal/util: godoc

--- internal/util/slices.go
+++ internal/util/slices.go
@@ -1,5 +1,9 @@
 package util
 
+// Last returns the last element from the provided slices. In other
+// words, if it is passed two slices and the second is empty, it will
+// return the last item of the first. ok is only false if all of the
+// provided slices are empty or no slices were provided.
 func Last[T any](s ...[]T) (v T, ok bool) {
 	for i := len(s) - 1; i >= 0; i-- {
 		if len(s[i]) > 0 {
