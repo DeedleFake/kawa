a94cdf4170213349a91a9a57984c0e7f1d1b27d7
DeedleFake <deedlefake@users.noreply.github.com>
2022-05-14 22:19:49 -0400

    theme: add

---
+++ theme/theme.go
@@ -0,0 +1,22 @@
+package theme
+
+import "image/color"
+
+const (
+	MinWidth  = 128
+	MinHeight = 24
+
+	WindowBorder = 5
+)
+
+var (
+	ColorBackground          = color.NRGBA{0x77, 0x77, 0x77, 0xFF}
+	ColorSelectionBox        = color.NRGBA{0xFF, 0x0, 0x0, 0xFF}
+	ColorSelectionBackground = color.NRGBA{0xFF, 0xFF, 0xFF, 0xFF / 100}
+	ColorActiveBorder        = color.NRGBA{0x50, 0xA1, 0xAD, 0xFF}
+	ColorInactiveBorder      = color.NRGBA{0x9C, 0xE9, 0xE9, 0xFF}
+	ColorMenuSelected        = color.NRGBA{0x3D, 0x7D, 0x42, 0xFF}
+	ColorMenuUnselected      = color.NRGBA{0xEB, 0xFF, 0xEC, 0xFF}
+	ColorMenuBorder          = color.NRGBA{0x78, 0xAD, 0x84, 0xFF}
+	ColorSurface             = color.NRGBA{0xEE, 0xEE, 0xEE, 0xFF}
+)
