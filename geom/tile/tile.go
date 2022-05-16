b0b574ba0bd41fde91d5e38151273836df912c58
DeedleFake <deedlefake@users.noreply.github.com>
2022-05-16 11:39:06 -0400

    geom/tile: add `TwoThirdsSidebar()` and `EvenVertically()`

--- geom/tile/tile.go
+++ geom/tile/tile.go
@@ -6,28 +6,33 @@ import (
 	"deedles.dev/kawa/geom"
 )
 
-// split splits a rectangle in two around the provided point. The
-// results are undefined if both the X and Y coordinates of the point
-// are non-zero.
-func split(r geom.Rect[float64], half geom.Point[float64]) (first, second geom.Rect[float64]) {
-	first = geom.Rect[float64]{Min: r.Min, Max: r.Max.Sub(half)}
-	second = geom.Rect[float64]{Min: r.Min.Add(half), Max: r.Max}
-	return
+// hsplit splits a rectangle into two rectangles arranged
+// horizontally.
+func hsplit(r geom.Rect[float64], w float64) (left, right geom.Rect[float64]) {
+	left = r.Resize(geom.Pt(w, r.Dy()))
+	right = r.Resize(geom.Pt(r.Dx()-w, r.Dy())).Add(geom.Pt(w, 0))
+	return left, right
 }
 
-// vsplit splits a rectangle in half vertically.
-func vsplit(r geom.Rect[float64]) (left, right geom.Rect[float64]) {
-	half := geom.Pt(r.Dx()/2, 0)
-	return split(r, half)
+func hsplitHalf(r geom.Rect[float64]) (left, right geom.Rect[float64]) {
+	return hsplit(r, r.Dx()/2)
 }
 
-// hsplit splits a rectangle in half horizontally.
-func hsplit(r geom.Rect[float64]) (top, bottom geom.Rect[float64]) {
-	half := geom.Pt(0, r.Dy()/2)
-	return split(r, half)
+// vsplit splits a rectangle into two rectangles arranged vertically.
+func vsplit(r geom.Rect[float64], h float64) (top, bottom geom.Rect[float64]) {
+	top = r.Resize(geom.Pt(r.Dx(), h))
+	bottom = r.Resize(geom.Pt(r.Dx(), r.Dy()-h)).Add(geom.Pt(0, h))
+	return top, bottom
 }
 
-// RightThenDown produces a series of n rectangles the union of which recomposes r. The rectangles are produced by splitting the right-most and then the bottom-most rectangles in half recusrively. In other words,
+func vsplitHalf(r geom.Rect[float64]) (top, bottom geom.Rect[float64]) {
+	return vsplit(r, r.Dy()/2)
+}
+
+// RightThenDown produces a series of n rectangles the union of which
+// recomposes r. The rectangles are produced by splitting the
+// right-most and then the bottom-most rectangles in half recusrively.
+// In other words,
 //
 //    RightThenDown(r, 4)
 //
@@ -40,13 +45,47 @@ func hsplit(r geom.Rect[float64]) (top, bottom geom.Rect[float64]) {
 //    ------------
 func RightThenDown(r geom.Rect[float64], n int) []geom.Rect[float64] {
 	tiles := make([]geom.Rect[float64], n)
+	return tiles
+}
+
+func rightThenDown(tiles []geom.Rect[float64], r geom.Rect[float64]) {
 	tiles[0] = r
 
-	split, next := vsplit, hsplit
+	split, next := hsplitHalf, vsplitHalf
 	for i := 1; i < len(tiles); i++ {
 		tiles[i-1], tiles[i] = split(tiles[i-1])
 		split, next = next, split
 	}
+}
 
+// TwoThirdsSidebar produces a layout where the first rectangle is
+// two-thirds the width of r and the rest are arranged vertically in
+// an even split in the remaining space.
+func TwoThirdsSidebar(r geom.Rect[float64], n int) []geom.Rect[float64] {
+	tiles := make([]geom.Rect[float64], n)
+	twoThirdsSidebar(tiles, r)
 	return tiles
+}
+
+func twoThirdsSidebar(tiles []geom.Rect[float64], r geom.Rect[float64]) {
+	var rem geom.Rect[float64]
+	tiles[0], rem = hsplit(r, 2*r.Dx()/3)
+	evenVertically(tiles[1:], rem)
+}
+
+// EvenVertically splits r into n rectangles arranged vertically each
+// with the full width of r.
+func EvenVertically(r geom.Rect[float64], n int) []geom.Rect[float64] {
+	tiles := make([]geom.Rect[float64], n)
+	evenVertically(tiles, r)
+	return tiles
+}
+
+func evenVertically(tiles []geom.Rect[float64], r geom.Rect[float64]) {
+	size := geom.Pt(0, r.Dy()/float64(len(tiles)))
+	c, _ := vsplit(r, size.Y)
+	for i := range tiles {
+		tiles[i] = c
+		c = c.Add(size)
+	}
 }
