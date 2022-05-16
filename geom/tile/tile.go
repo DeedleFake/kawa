1664565d85bb02e183d7d627bd4956fcb0d837ae
DeedleFake <deedlefake@users.noreply.github.com>
2022-05-16 11:45:43 -0400

    geom/tile: don't hardcode to `float64`
    `constraints.Scalar` should definitely be a thing...

--- geom/tile/tile.go
+++ geom/tile/tile.go
@@ -4,28 +4,29 @@ package tile
 
 import (
 	"deedles.dev/kawa/geom"
+	"golang.org/x/exp/constraints"
 )
 
 // hsplit splits a rectangle into two rectangles arranged
 // horizontally.
-func hsplit(r geom.Rect[float64], w float64) (left, right geom.Rect[float64]) {
+func hsplit[T constraints.Integer | constraints.Float](r geom.Rect[T], w T) (left, right geom.Rect[T]) {
 	left = r.Resize(geom.Pt(w, r.Dy()))
 	right = r.Resize(geom.Pt(r.Dx()-w, r.Dy())).Add(geom.Pt(w, 0))
 	return left, right
 }
 
-func hsplitHalf(r geom.Rect[float64]) (left, right geom.Rect[float64]) {
+func hsplitHalf[T constraints.Integer | constraints.Float](r geom.Rect[T]) (left, right geom.Rect[T]) {
 	return hsplit(r, r.Dx()/2)
 }
 
 // vsplit splits a rectangle into two rectangles arranged vertically.
-func vsplit(r geom.Rect[float64], h float64) (top, bottom geom.Rect[float64]) {
+func vsplit[T constraints.Integer | constraints.Float](r geom.Rect[T], h T) (top, bottom geom.Rect[T]) {
 	top = r.Resize(geom.Pt(r.Dx(), h))
 	bottom = r.Resize(geom.Pt(r.Dx(), r.Dy()-h)).Add(geom.Pt(0, h))
 	return top, bottom
 }
 
-func vsplitHalf(r geom.Rect[float64]) (top, bottom geom.Rect[float64]) {
+func vsplitHalf[T constraints.Integer | constraints.Float](r geom.Rect[T]) (top, bottom geom.Rect[T]) {
 	return vsplit(r, r.Dy()/2)
 }
 
@@ -43,15 +44,15 @@ func vsplitHalf(r geom.Rect[float64]) (top, bottom geom.Rect[float64]) {
 //    |    -------
 //    |    |  |  |
 //    ------------
-func RightThenDown(r geom.Rect[float64], n int) []geom.Rect[float64] {
-	tiles := make([]geom.Rect[float64], n)
+func RightThenDown[T constraints.Integer | constraints.Float](r geom.Rect[T], n int) []geom.Rect[T] {
+	tiles := make([]geom.Rect[T], n)
 	return tiles
 }
 
-func rightThenDown(tiles []geom.Rect[float64], r geom.Rect[float64]) {
+func rightThenDown[T constraints.Integer | constraints.Float](tiles []geom.Rect[T], r geom.Rect[T]) {
 	tiles[0] = r
 
-	split, next := hsplitHalf, vsplitHalf
+	split, next := hsplitHalf[T], vsplitHalf[T]
 	for i := 1; i < len(tiles); i++ {
 		tiles[i-1], tiles[i] = split(tiles[i-1])
 		split, next = next, split
@@ -61,28 +62,28 @@ func rightThenDown(tiles []geom.Rect[float64], r geom.Rect[float64]) {
 // TwoThirdsSidebar produces a layout where the first rectangle is
 // two-thirds the width of r and the rest are arranged vertically in
 // an even split in the remaining space.
-func TwoThirdsSidebar(r geom.Rect[float64], n int) []geom.Rect[float64] {
-	tiles := make([]geom.Rect[float64], n)
+func TwoThirdsSidebar[T constraints.Integer | constraints.Float](r geom.Rect[T], n int) []geom.Rect[T] {
+	tiles := make([]geom.Rect[T], n)
 	twoThirdsSidebar(tiles, r)
 	return tiles
 }
 
-func twoThirdsSidebar(tiles []geom.Rect[float64], r geom.Rect[float64]) {
-	var rem geom.Rect[float64]
+func twoThirdsSidebar[T constraints.Integer | constraints.Float](tiles []geom.Rect[T], r geom.Rect[T]) {
+	var rem geom.Rect[T]
 	tiles[0], rem = hsplit(r, 2*r.Dx()/3)
 	evenVertically(tiles[1:], rem)
 }
 
 // EvenVertically splits r into n rectangles arranged vertically each
 // with the full width of r.
-func EvenVertically(r geom.Rect[float64], n int) []geom.Rect[float64] {
-	tiles := make([]geom.Rect[float64], n)
+func EvenVertically[T constraints.Integer | constraints.Float](r geom.Rect[T], n int) []geom.Rect[T] {
+	tiles := make([]geom.Rect[T], n)
 	evenVertically(tiles, r)
 	return tiles
 }
 
-func evenVertically(tiles []geom.Rect[float64], r geom.Rect[float64]) {
-	size := geom.Pt(0, r.Dy()/float64(len(tiles)))
+func evenVertically[T constraints.Integer | constraints.Float](tiles []geom.Rect[T], r geom.Rect[T]) {
+	size := geom.Pt(0, r.Dy()/T(len(tiles)))
 	c, _ := vsplit(r, size.Y)
 	for i := range tiles {
 		tiles[i] = c
