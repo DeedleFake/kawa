416bdf32878ac60ede957540bddcd13e69bca624
DeedleFake <deedlefake@users.noreply.github.com>
2023-02-27 17:44:58 -0500

    kawa: use `XDGDecorationManagerV1`

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module deedles.dev/kawa
 go 1.18
 
 require (
-	deedles.dev/wlr v0.0.0-20230227213316-c324a47815eb
+	deedles.dev/wlr v0.0.0-20230227223056-23e34f832eaf
 	deedles.dev/ximage v0.0.0-20230216200226-85a1bdca6dc7
 	golang.org/x/exp v0.0.0-20230224173230-c95f2b4c22f2
 	golang.org/x/image v0.5.0
--- server.go
+++ server.go
@@ -42,7 +42,7 @@ type Server struct {
 	xdgShell          wlr.XDGShell
 	layerShell        wlr.LayerShellV1
 	xwayland          wlr.XWayland
-	decorationManager wlr.ServerDecorationManager
+	decorationManager wlr.XDGDecorationManagerV1
 
 	outputs     []*Output
 	inputs      []wlr.InputDevice
@@ -64,18 +64,18 @@ type Server struct {
 
 	inputMode InputMode
 
-	onNewOutputListener            wlr.Listener
-	onNewInputListener             wlr.Listener
-	onCursorMotionListener         wlr.Listener
-	onCursorMotionAbsoluteListener wlr.Listener
-	onCursorButtonListener         wlr.Listener
-	onCursorAxisListener           wlr.Listener
-	onCursorFrameListener          wlr.Listener
-	onRequestCursorListener        wlr.Listener
-	onNewXDGSurfaceListener        wlr.Listener
-	onNewXWaylandSurfaceListener   wlr.Listener
-	onNewLayerSurfaceListener      wlr.Listener
-	onNewDecorationListener        wlr.Listener
+	onNewOutputListener             wlr.Listener
+	onNewInputListener              wlr.Listener
+	onCursorMotionListener          wlr.Listener
+	onCursorMotionAbsoluteListener  wlr.Listener
+	onCursorButtonListener          wlr.Listener
+	onCursorAxisListener            wlr.Listener
+	onCursorFrameListener           wlr.Listener
+	onRequestCursorListener         wlr.Listener
+	onNewXDGSurfaceListener         wlr.Listener
+	onNewXWaylandSurfaceListener    wlr.Listener
+	onNewLayerSurfaceListener       wlr.Listener
+	onNewToplevelDecorationListener wlr.Listener
 }
 
 func (server *Server) Release() {
@@ -90,7 +90,7 @@ func (server *Server) Release() {
 	server.onNewXDGSurfaceListener.Destroy()
 	server.onNewXWaylandSurfaceListener.Destroy()
 	server.onNewLayerSurfaceListener.Destroy()
-	server.onNewDecorationListener.Destroy()
+	server.onNewToplevelDecorationListener.Destroy()
 }
 
 func (server *Server) Shutdown() {
